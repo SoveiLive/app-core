b249e0e4cbecf74e61c068a224636c92732a3444
6346563751 <b0bb1e@protonmail.com>
2023-07-04 22:25:05 +0200

    feat: remove useless comment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   publicRuntimeConfig: {
-    // Will be available on both server and client
     DATABASE: '0x38801fd445898d1851dcf8ed5504840a98434800',
     ROLLUP_NODE: 'https://rollup.cloud.db3.network',
     INDEX_NODE: 'https://index.cloud.db3.network'
-    //"http://127.0.0.1:26619",
-  	//"http://127.0.0.1:26639",
   },
 }
