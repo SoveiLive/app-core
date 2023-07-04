66e2915c16d580a1440075a88c8e534465b4826e
6346563751 <b0bb1e@protonmail.com>
2023-07-04 22:41:04 +0200

    feat: change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "SoveiLive - The social media",
+  title: "SoveiLive - The social media platform",
   description:
-    "The social media where you really control your data, thanks to web3.",
+    "The social media platform where you really control your data, thanks to web3.",
 };
 
 import Page from "./app";
